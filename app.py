bf8725da392a6fa61ce2e8c0ff8f4a2fe5f4fce2
SomeRandomPythonLearner <arloelliot1@gmail.com>
2025-05-07 19:11:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def calculate_cost():
         'index.html',
         coins=round(session['coins'], 2),
         value=round(session['current_cost'], 6),
-        confirm_cost=round(cost, 4),
+        confirm_cost=cost
         mine_count=mine_count
     )
 
